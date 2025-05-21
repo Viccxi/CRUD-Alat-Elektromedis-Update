7b2df07a6050f29e4f848ab08d6dce75480e2aae
Ibrahimovic Ilham Prastama <ovicilham123@gmail.com>
2025-05-21 21:26:02 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost'; 
+$databaseName = 'simrs';
+$databaseUsername = 'root';
+$databasePassword = ''; // atau isikan sesuai password MySQL kamu
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+?>
